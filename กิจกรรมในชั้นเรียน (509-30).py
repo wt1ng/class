bb3550d24a9ca64accd85d724a8d8e8f3468c092
wt1ng <apitsadabaitongg@gmail.com>
2024-12-02 14:54:01 +0700

    Add files via upload

---
+++ กิจกรรมในชั้นเรียน (509-30).py
@@ -0,0 +1,9 @@
+province = ["กรุงเทพมหานคร","กาฬสินธุ์","กาญจนบุรี","จันทบุรี","ฉะเชิงเทรา","ชลบุรี","พะเยา","ชุมพร","ชัยนาท","ขอนแก่น","ชัยภูมิ","ยโสธร","นครพนม","นครราชสีมา","เชียงใหม่"]
+
+province[0] = "Bangkok"
+province[13] = "Korat"
+province[4] = "Paet Riw"
+province[5] = "Chon buri"
+province[7] = "Chum porn"
+
+print(province)
